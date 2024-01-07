a96e4afdd4ed8050fccd6038051da97066c5afa0
артур <okolobaxha@yandex.ru>
2024-01-07 22:35:00 +0700

    init

---
+++ src/main/java/org/ex1de/dropitem/Dropitem.java
@@ -0,0 +1,17 @@
+package org.ex1de.dropitem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dropitem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
