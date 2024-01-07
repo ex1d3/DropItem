dcc6e18ff6be98ea1f541ad5038479e9b782e4bd
артур <okolobaxha@yandex.ru>
2024-01-08 02:16:25 +0700

    add: PlayerMemory specified

--- src/main/java/org/ex1de/dropitem/core/player/playermemory/PlayerMemory.java
+++ src/main/java/org/ex1de/dropitem/core/player/playermemory/PlayerMemory.java
@@ -1,6 +1,43 @@
 package org.ex1de.dropitem.core.player.playermemory;
 
-import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
+import java.io.Serializable;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Getter
+@Setter
 public class PlayerMemory implements Serializable {
+    private Map<Material, Integer> dropItems;
+
+    public PlayerMemory() {
+        this.dropItems = new EnumMap<>(Material.class);
+    }
+
+    @NotNull
+    public static NamespacedKey getNamespacedKey(JavaPlugin plugin) {
+        return new NamespacedKey(
+                plugin,
+                PersistentPlayerMemory.TOKEN
+        );
+    }
+
+    @NotNull
+    public static String getPlayerFolderPath(
+            UUID playerId,
+            @NotNull JavaPlugin plugin
+    ) {
+        return String.format(
+                "%s/%s",
+                plugin.getDataFolder().getPath(),
+                playerId
+        );
+    }
 }
