d5780dfeb8272f7574a4c6d083d96579055c4b86
артур <okolobaxha@yandex.ru>
2024-01-08 02:18:36 +0700

    add: PlayerUtility specified

--- src/main/java/org/ex1de/dropitem/core/player/PlayerUtility.java
+++ src/main/java/org/ex1de/dropitem/core/player/PlayerUtility.java
@@ -1,4 +1,40 @@
 package org.ex1de.dropitem.core.player;
 
-public class PlayerUtility {
+import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.ex1de.dropitem.core.player.playermemory.PersistentPlayerMemory;
+import org.ex1de.dropitem.core.player.playermemory.PlayerMemory;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public final class PlayerUtility {
+    private PlayerUtility() {  }
+
+    @NotNull
+    public static PlayerMemory getMemory(
+            @NotNull Player p,
+            @NotNull JavaPlugin plugin) {
+        PersistentDataContainer data = p.getPersistentDataContainer();
+
+        return Objects.requireNonNull(data.get(
+                PlayerMemory.getNamespacedKey(plugin),
+                new PersistentPlayerMemory()
+        ));
+    }
+
+    public static void setMemory(
+            @NotNull Player p,
+            @NotNull PlayerMemory pMemory,
+            @NotNull JavaPlugin plugin
+    ) {
+        PersistentDataContainer data = p.getPersistentDataContainer();
+
+        data.set(
+                PlayerMemory.getNamespacedKey(plugin),
+                new PersistentPlayerMemory(),
+                pMemory
+        );
+    }
 }
