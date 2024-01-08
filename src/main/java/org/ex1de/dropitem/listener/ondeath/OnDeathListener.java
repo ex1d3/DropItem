d5e190992e0e0b6562ad9a2b69e0758cb1eb8154
артур <okolobaxha@yandex.ru>
2024-01-08 15:11:01 +0700

    feat (listener): dropping items from dropList to ground of player's death place

--- src/main/java/org/ex1de/dropitem/listener/ondeath/OnDeathListener.java
+++ src/main/java/org/ex1de/dropitem/listener/ondeath/OnDeathListener.java
@@ -1,6 +1,9 @@
 package org.ex1de.dropitem.listener.ondeath;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class OnDeathListener implements Listener {
@@ -8,5 +11,13 @@ public class OnDeathListener implements Listener {
 
     public OnDeathListener(final JavaPlugin plugin) {
         onDeathService = new OnDeathService(plugin);
+    }
+
+    @EventHandler
+    public boolean onDeathHandler(PlayerDeathEvent e) {
+        Player p = e.getPlayer();
+        onDeathService.dropItemsFromDropList(p);
+
+        return true;
     }
 }
