a58e1e06a0b119fe8bd0458b02d3ac349b5416cf
артур <okolobaxha@yandex.ru>
2024-01-08 14:40:08 +0700

    feat (listener): write PlayerMemory to player's file on player quit.

--- src/main/java/org/ex1de/dropitem/listener/onquit/OnQuitService.java
+++ src/main/java/org/ex1de/dropitem/listener/onquit/OnQuitService.java
@@ -1,11 +1,24 @@
 package org.ex1de.dropitem.listener.onquit;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.ex1de.dropitem.core.player.PlayerUtility;
+import org.ex1de.dropitem.core.player.playermemory.PlayerMemory;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
 
 public class OnQuitService {
     private final JavaPlugin plugin;
 
     public OnQuitService(final JavaPlugin pluginToApply) {
         plugin = pluginToApply;
+    }
+
+    public void savePlayerData(@NotNull Player p) {
+        UUID pId = p.getUniqueId();
+        PlayerMemory pMemory = PlayerUtility.getMemory(p, plugin);
+
+        PlayerUtility.savePlayerMemoryToFile(pId, pMemory, plugin);
     }
 }
